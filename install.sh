eeeafa8bcaa9a40efc599f28a7fda4a85cb1c1c0
root <root@81744.local>
2017-09-20 15:31:37 +0000

    add cpulimit

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev git vim supervisor cpulimit
+
 mkdir /root/1
 cp ./xmr-stak-cpu /root/1/xmr-stak-cpu
 cp ./config.txt /root/1/config.txt
+
+mkdir /root/2
+cp ./xmr-stak-cpu /root/2/xmr-stak-cpu
+cp ./config2.txt /root/2/config.txt
+
 sysctl -w vm.nr_hugepages=128
 ulimit -l
 cp -rf ./limits.conf /etc/security/limits.conf
+
 cp -rf ./supervisord.conf /etc/supervisor/supervisord.conf
-/etc/init.d/supervisor restart all 
+/etc/init.d/supervisor restart all
+
+cp -rf ./rc.local /etc/rc.local
+/etc/rc.local
