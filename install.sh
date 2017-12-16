770f93d017feef4e2c32ea0756237f0e84ab2691
merzota <34197901+merzota@users.noreply.github.com>
2017-12-16 12:31:39 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,16 @@
 apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev git vim supervisor cpulimit
 
 mkdir /root/1
-cp xn/xmr-stak-cpu /root/1/xmr-stak-cpu
-cp xn/config.txt /root/1/config.txt
+cp /xn/xmr-stak-cpu /root/1/xmr-stak-cpu
+cp /xn/config.txt /root/1/config.txt
 
 mkdir /root/2
-cp xn/xmr-stak-cpu /root/2/xmr-stak-cpu
-cp xn/config2.txt /root/2/config.txt
+cp /xn/xmr-stak-cpu /root/2/xmr-stak-cpu
+cp /xn/config2.txt /root/2/config.txt
 
 sysctl -w vm.nr_hugepages=128
 ulimit -l
-cp -rf xn/limits.conf /etc/security/limits.conf
+cp -rf /xn/limits.conf /etc/security/limits.conf
 
 cp -rf xn/supervisord.conf /etc/supervisor/supervisord.conf
 /etc/init.d/supervisor restart all
